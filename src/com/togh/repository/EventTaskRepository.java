e8234b6c29882f5ad8e92eaff5be48a91aa96a0d
Pierre-Yves-Monnet <pierre-yves.monnet@laposte.net>
2021-03-27 16:04:46 -0700

    Add comments

--- src/com/togh/repository/EventTaskRepository.java
+++ src/com/togh/repository/EventTaskRepository.java
@@ -1,8 +1,25 @@
+/* ******************************************************************************** */
+/*                                                                                  */
+/*  Togh Project                                                                    */
+/*                                                                                  */
+/*  This component is part of the Togh Project, developed by Pierre-Yves Monnet     */
+/*                                                                                  */
+/*                                                                                  */
+/* ******************************************************************************** */
 package com.togh.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.togh.entity.EventTaskEntity;
+
+/* ******************************************************************************** */
+/*                                                                                  */
+/*  EventTaskRepository                                                             */
+/*                                                                                  */
+/*  Control what's happen on an event. Pilot all operations                         */
+/*                                                                                  */
+/*                                                                                  */
+/* ******************************************************************************** */
 
 public interface EventTaskRepository extends JpaRepository<EventTaskEntity, Long>  {
 
